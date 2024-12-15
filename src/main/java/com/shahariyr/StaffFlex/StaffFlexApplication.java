b48fc0cee9793c18f33a9d945d3804f870a0089c
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-12-15 22:49:00 +0600

    Initial commit

---
+++ src/main/java/com/shahariyr/StaffFlex/StaffFlexApplication.java
@@ -0,0 +1,13 @@
+package com.shahariyr.StaffFlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffFlexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahariyr/StaffFlex/StaffFlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahariyr.StaffFlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffFlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
