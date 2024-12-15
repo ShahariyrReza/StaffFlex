0a686baaab7dfec7aac6af3fba99ca87502659db
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-12-15 23:40:54 +0600

    ...

---
+++ src/main/java/com/shahariyr/StaffFlex/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.shahariyr.StaffFlex.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integere> {
+
+}
