282b3ddde39264941cb3a771b4c6a3c635506207
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-12-18 19:14:25 +0600

    upfate a class

--- src/main/java/com/shahariyr/StaffFlex/controller/EmployeeController.java
+++ src/main/java/com/shahariyr/StaffFlex/controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.shahariyr.StaffFlex.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -9,41 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shahariyr.StaffFlex.entity.Employee;
-
-import jakarta.annotation.PostConstruct;
+import com.shahariyr.StaffFlex.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	private List<Employee> theEmployees;
+	private EmployeeService employeeService;
 
-	@PostConstruct
-	private void loadData() {
-
-		// create employees
-		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@gmail.com");
-		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@hotmail.com");
-		Employee emp3 = new Employee("Avani", "Gupta", "avani@yahoo.com");
-
-		// create the list
-		theEmployees = new ArrayList<>();
-
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	public EmployeeController(EmployeeService theEmployeeService) {
+		this.employeeService = theEmployeeService;
 	}
 
 	// add mapping for "/list"
-
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 
+		List<Employee> theEmployees = employeeService.findAll();
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
 		return "list-employees";
 	}
-	
+
 }
