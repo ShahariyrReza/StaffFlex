e952feb903f953b66921357e5871134f6bff6294
Shahariyr reza <shahariyr.reza@hotmail.com>
2025-01-07 23:17:42 +0600

    .

--- sql-scripts/employee-directory.sql
+++ sql-scripts/employee-directory.sql
@@ -12,6 +12,7 @@ CREATE TABLE `employee` (
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
@@ -20,9 +21,10 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'shahariyr','reza','shahariyr.reza@hotmail.com'),
-	(2,'Emma','Watson','emma.watson@hotmail.com'),
-	(3,'Shibber','Ahmmed','shibber.ahmmed@yahoo.com'),
-	(4,'Akash','Sarker','akash.sarker@gmail.com'),
-	(5,'Mazher','Reza','mazher.reza@gmail.com');
+	(1,'shahariyr','reza','shahariyr.reza@hotmail.com','01703265352'),
+	(2,'Emma','Watson','emma.watson@hotmail.com','01703265352'),
+	(3,'Shibber','Ahmmed','shibber.ahmmed@yahoo.com','01703265352'),
+	(4,'Akash','Sarker','akash.sarker@gmail.com','01703265352'),
+	(5,'Mazher','Reza','mazher.reza@gmail.com','01703265352')
+
 
