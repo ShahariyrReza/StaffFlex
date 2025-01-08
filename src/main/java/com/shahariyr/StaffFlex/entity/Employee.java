e4e1361ea17d6dd07faf208eeabc29ccb10460ff
Shahariyr reza <shahariyr.reza@hotmail.com>
2025-01-09 00:01:20 +0600

    update entity

--- src/main/java/com/shahariyr/StaffFlex/entity/Employee.java
+++ src/main/java/com/shahariyr/StaffFlex/entity/Employee.java
@@ -8,148 +8,108 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class Employee {
 
 	// define fields
-	
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
-	
-	@Column(name="first_name")
+
+	@Column(name = "first_name")
 	private String firstName;
-	
-	@Column(name="last_name")
+
+	@Column(name = "last_name")
 	private String lastName;
-	
-	@Column(name="email")
+
+	@Column(name = "email")
 	private String email;
-	
+
 	@Column(name = "mobile")
 	private String mobileNumber;
 
-	
-	
-		
+	@Column(name = "address")
+	private String address;
+
 	// define no argument constructors
 
 	public Employee() {
 		super();
 	}
 
-
-
-
-	public Employee(String firstName, String lastName, String email, String mobileNumber) {
+	public Employee(String firstName, String lastName, String email, String mobileNumber, String address) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.mobileNumber = mobileNumber;
+		this.address = address;
 	}
 
-
-
-
-	public Employee(int id, String firstName, String lastName, String email, String mobileNumber) {
+	public Employee(int id, String firstName, String lastName, String email, String mobileNumber, String address) {
 		super();
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.mobileNumber = mobileNumber;
+		this.address = address;
 	}
-
 
 	public int getId() {
 		return id;
 	}
 
-
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
-
 
 	public String getFirstName() {
 		return firstName;
 	}
 
-
-
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
-
-
 
 	public String getLastName() {
 		return lastName;
 	}
 
-
-
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
-
 
 	public String getMobileNumber() {
 		return mobileNumber;
 	}
 
-
-
-
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
 
+	public String getAddress() {
+		return address;
+	}
 
-
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", mobileNumber=" + mobileNumber + "]";
+				+ ", mobileNumber=" + mobileNumber + ", address=" + address + "]";
 	}
-	
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
